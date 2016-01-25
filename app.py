5230f9f330645a112e8666d05fb1bdae6ddfbe79
Allan Lasser <lasser.allan@gmail.com>
2016-01-24 20:01:29 -0500

    integrates postgres

--- app.py
+++ app.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
